285500ab40f29ab13ad4be022f7bd3ec6769af8a
Amselber <amselber@gmail.com>
2026-06-17 17:53:02 +0300

    Update [main]: new [cli]

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 // src/main.c
 #include "cli.h"
-#include "commands.h"
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-  register_all_commands();
-  cli_run(argc, argv);
+  if (argc > 1 && !strcmp(argv[1], "help")) {
+    cli_show_help();
+    return 0;
+  }
+  if (argc > 1 && !strcmp(argv[1], "version")) {
+    printf("0.0.1\n");
+  }
 
   return 0;
 }
