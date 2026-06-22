c4e40fc6d4af9fc7bbce04ec9e772f957884493f
Amselber <amselber@gmail.com>
2026-06-22 15:54:23 +0300

    fix(log): fix log_errno, change __VA_ARGS__ with C99 Standart

--- src/log.h
+++ src/log.h
@@ -2,6 +2,8 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <errno.h>
+
 typedef enum {
   LOG_DEBUG,
   LOG_INFO,
@@ -13,17 +15,18 @@ typedef enum {
 int log_init(void);
 
 #define log_debug(...)                                                         \
-  _log_write(LOG_DEBUG, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+  _log_write(LOG_DEBUG, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define log_info(...)                                                          \
-  _log_write(LOG_INFO, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+  _log_write(LOG_INFO, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define log_warn(...)                                                          \
-  _log_write(LOG_WARN, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+  _log_write(LOG_WARN, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define log_error(...)                                                         \
-  _log_write(LOG_ERROR, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+  _log_write(LOG_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define log_fatal(...)                                                         \
-  _log_write(LOG_FATAL, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+  _log_write(LOG_FATAL, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define log_errno(...)                                                         \
-  log_error(" [errno=%d: %s]", ##__VA_ARGS__, errno, strerror(errno))
+  _log_write(LOG_FATAL, __FILE__, __LINE__, __func__, "%s [errno=%d: %s]",     \
+             __VA_ARGS__, errno, strerror(errno))
 
 void _log_write(log_level_t level, const char *file, int line, const char *func,
                 const char *fmt, ...) __attribute__((format(printf, 5, 6)));
