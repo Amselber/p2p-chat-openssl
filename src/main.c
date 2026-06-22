bcd8287705416648f2d425d2404087cf18a971fe
Amselber <amselber@gmail.com>
2026-06-22 12:30:01 +0300

    feat: add log module and version banner

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 // src/main.c
 #include "cli.h"
+#include "config.h"
+#include "log.h"
+#include "version.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -9,8 +12,18 @@ int main(int argc, char **argv) {
     return 0;
   }
   if (argc > 1 && !strcmp(argv[1], "version")) {
-    printf("0.0.1\n");
+    printf("Version: %s\n", VERSION_STRING);
   }
+
+  config_load("p2pchat.ini");
+  strcpy(g_config.log_level, "debug");
+  g_config.log_to_console = 1;
+  g_config.log_file[0] = '\0';
+  log_init();
+
+  log_info("==== P2P Chat starting ====");
+  log_debug("Multicast: %s:%u", g_config.multicast_addr,
+            g_config.multicast_port);
 
   return 0;
 }
