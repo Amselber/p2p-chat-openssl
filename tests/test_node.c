f221a2451f081e49f8c2e2d23caff7e7d055e3fb
Amselber <amselber@gmail.com>
2026-06-25 16:05:40 +0300

    test(node): update tests after adding 'name' feature

--- tests/test_node.c
+++ tests/test_node.c
@@ -7,11 +7,16 @@
 #define FP2 "b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3"
 #define FP3 "c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4"
 
+// Имена
+#define FP1_NAME "alice"
+#define FP2_NAME "bob"
+#define FP3_NAME "marty"
+
 void setUp_node(void) { node_reset(); }
 
 // Тест: добавление новой ноды
 void test_node_add_new(void) {
-  node_t *n = node_add(FP1, 5);
+  node_t *n = node_add(FP1, FP1_NAME, 5);
   TEST_ASSERT_NOT_NULL(n);
   TEST_ASSERT_EQUAL_STRING(FP1, n->fp);
   TEST_ASSERT_EQUAL(5, n->fd);
@@ -19,13 +24,15 @@ void test_node_add_new(void) {
 
 // Тест: обновление существующей ноды
 void test_node_add_update(void) {
-  node_t *n1 = node_add(FP1, 5);
+  node_t *n1 = node_add(FP1, FP1_NAME, 5);
   TEST_ASSERT_NOT_NULL(n1);
+  TEST_ASSERT_EQUAL_STRING(FP1_NAME, n1->name);
   TEST_ASSERT_EQUAL(5, n1->fd);
 
   // Добавляем ту же ноду с новым fd
-  node_t *n2 = node_add(FP1, 7);
+  node_t *n2 = node_add(FP1, FP1_NAME, 7);
   TEST_ASSERT_NOT_NULL(n2);
+  TEST_ASSERT_EQUAL_STRING(FP1_NAME, n1->name);
   TEST_ASSERT_EQUAL(7, n2->fd);
 
   // Это должен быть тот же указатель
@@ -34,31 +41,34 @@ void test_node_add_update(void) {
 
 // Тест: две разные ноды
 void test_node_add_two_different(void) {
-  node_t *a = node_add(FP1, 5);
-  node_t *b = node_add(FP2, 6);
+  node_t *a = node_add(FP1, FP1_NAME, 5);
+  node_t *b = node_add(FP2, FP2_NAME, 6);
 
   TEST_ASSERT_NOT_NULL(a);
   TEST_ASSERT_NOT_NULL(b);
   TEST_ASSERT_NOT_EQUAL(a, b);
   TEST_ASSERT_EQUAL_STRING(FP1, a->fp);
   TEST_ASSERT_EQUAL_STRING(FP2, b->fp);
+  TEST_ASSERT_EQUAL_STRING(FP1_NAME, a->name);
+  TEST_ASSERT_EQUAL_STRING(FP2_NAME, b->name);
   TEST_ASSERT_EQUAL(5, a->fd);
   TEST_ASSERT_EQUAL(6, b->fd);
 }
 
 // Тест: find_by_fd находит
 void test_node_find_by_fd_found(void) {
-  node_add(FP1, 10);
-  node_add(FP2, 20);
+  node_add(FP1, FP1_NAME, 10);
+  node_add(FP2, FP2_NAME, 20);
 
   node_t *n = node_find_by_fd(20);
   TEST_ASSERT_NOT_NULL(n);
   TEST_ASSERT_EQUAL_STRING(FP2, n->fp);
+  TEST_ASSERT_EQUAL_STRING(FP2_NAME, n->name);
 }
 
 // Тест: find_by_fd не находит
 void test_node_find_by_fd_not_found(void) {
-  node_add(FP1, 10);
+  node_add(FP1, FP1_NAME, 10);
 
   node_t *n = node_find_by_fd(99);
   TEST_ASSERT_NULL(n);
@@ -66,11 +76,12 @@ void test_node_find_by_fd_not_found(void) {
 
 // Тест: find_by_fd с -1
 void test_node_find_by_fd_minus_one(void) {
-  node_add(FP1, -1);
+  node_add(FP1, FP1_NAME, -1);
 
   node_t *n = node_find_by_fd(-1);
   TEST_ASSERT_NOT_NULL(n);
   TEST_ASSERT_EQUAL_STRING(FP1, n->fp);
+  TEST_ASSERT_EQUAL_STRING(FP1_NAME, n->name);
 }
 
 // Вспомогательная: сбор fp в буфер через node_each
@@ -82,9 +93,9 @@ static void collect_fp(node_t *n, void *arg) {
 
 // Тест: node_each обходит все
 void test_node_each_iterates_all(void) {
-  node_add(FP1, 1);
-  node_add(FP2, 2);
-  node_add(FP3, 3);
+  node_add(FP1, FP1_NAME, 1);
+  node_add(FP2, FP2_NAME, 2);
+  node_add(FP3, FP3_NAME, 3);
 
   char buf[256] = {0};
   node_each(collect_fp, buf);
@@ -108,9 +119,10 @@ void test_node_each_empty(void) {
 
 // Тест: fd = -1 (нода не подключена)
 void test_node_fd_minus_one(void) {
-  node_t *n = node_add(FP1, -1);
+  node_t *n = node_add(FP1, FP1_NAME, -1);
   TEST_ASSERT_NOT_NULL(n);
   TEST_ASSERT_EQUAL(-1, n->fd);
+  TEST_ASSERT_EQUAL_STRING(FP1_NAME, n->name);
 }
 
 int main_test_node(void) {
